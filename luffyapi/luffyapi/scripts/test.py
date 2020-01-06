6406a40451433c4ab7d9896c416c0b8276020238
setcreed <2441767409@qq.com>
2020-01-06 14:48:06 +0800

    modify .gitignore

--- luffyapi/luffyapi/scripts/test.py
+++
@@ -1,9 +0,0 @@
-import os, django
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "luffyapi.settings.dev")
-django.setup()
-
-from home import views
-print(views)
-
-# from utils.logging import logger
-# logger.critical('asf')
